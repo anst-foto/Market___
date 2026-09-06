290010b762fa5f5647d980036b97b89fb8d2c232
Starinin Andrey <starinin-andrey@ya.ru>
2026-09-06 12:55:18 +0300

    Init

---
+++ Market.Api/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello from Minimal API!");
+
+app.Run();
